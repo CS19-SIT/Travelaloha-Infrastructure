d8b646223fecc2ee28412a1407ca7b924829b389
ThanadonTee <thanadon.rh@mail.kmutt.ac.th>
2019-10-15 19:29:06 +0700

    init Commit

---
+++ create_user.sh
@@ -0,0 +1,274 @@
+#!/bin/bash
+
+# Add user
+
+adduser --disabled-password --gecos "" natchaphon.raj --force-badname
+adduser --disabled-password --gecos "" thanyawee.sri --force-badname
+adduser --disabled-password --gecos "" yasumin.ker --force-badname
+adduser --disabled-password --gecos "" nachol.tae --force-badname
+adduser --disabled-password --gecos "" benjaporn.bhu --force-badname
+adduser --disabled-password --gecos "" athisri.sit --force-badname
+adduser --disabled-password --gecos "" kritamate.kam --force-badname
+adduser --disabled-password --gecos "" krittiya.onb --force-badname
+adduser --disabled-password --gecos "" kantapon.ruj --force-badname
+adduser --disabled-password --gecos "" krerkthad.kun --force-badname
+adduser --disabled-password --gecos "" kiatisak.pet --force-badname
+adduser --disabled-password --gecos "" charuphat.chi --force-badname
+adduser --disabled-password --gecos "" chaichet.pha --force-badname
+adduser --disabled-password --gecos "" nuttawut.pro --force-badname
+adduser --disabled-password --gecos "" natthida.tha --force-badname
+adduser --disabled-password --gecos "" tawan.von --force-badname
+adduser --disabled-password --gecos "" taipun.ban --force-badname
+adduser --disabled-password --gecos "" tanadon.say --force-badname
+adduser --disabled-password --gecos "" teeranai.kon --force-badname
+adduser --disabled-password --gecos "" tianwit.mou --force-badname
+adduser --disabled-password --gecos "" preeyawarang.pon --force-badname
+adduser --disabled-password --gecos "" pongpoom.pan --force-badname
+adduser --disabled-password --gecos "" patthamaporn.sam --force-badname
+adduser --disabled-password --gecos "" pongsakorn.cha --force-badname
+adduser --disabled-password --gecos "" phruksachat.yao --force-badname
+adduser --disabled-password --gecos "" pittawat.tav --force-badname
+adduser --disabled-password --gecos "" phuritat.cha --force-badname
+adduser --disabled-password --gecos "" vorapol.cha --force-badname
+adduser --disabled-password --gecos "" vorawut.per --force-badname
+adduser --disabled-password --gecos "" warakorn.cha --force-badname
+adduser --disabled-password --gecos "" waritnan.soo --force-badname
+adduser --disabled-password --gecos "" wisa.moo --force-badname
+adduser --disabled-password --gecos "" sakdipat.ont --force-badname
+adduser --disabled-password --gecos "" supapatr.won --force-badname
+adduser --disabled-password --gecos "" anawat.pao --force-badname
+adduser --disabled-password --gecos "" audsadawut.suk --force-badname
+adduser --disabled-password --gecos "" aekknon.pho --force-badname
+adduser --disabled-password --gecos "" kongphob.won --force-badname
+adduser --disabled-password --gecos "" kani-on.pan --force-badname
+adduser --disabled-password --gecos "" chantisa.san --force-badname
+adduser --disabled-password --gecos "" thitaree.kem --force-badname
+adduser --disabled-password --gecos "" napat.jam --force-badname
+adduser --disabled-password --gecos "" thanadon.tee --force-badname
+adduser --disabled-password --gecos "" noppanut.boo --force-badname
+adduser --disabled-password --gecos "" preeyapat.sut --force-badname
+adduser --disabled-password --gecos "" pornphat.sro --force-badname
+adduser --disabled-password --gecos "" wicharn.rue --force-badname
+adduser --disabled-password --gecos "" sinin.cha --force-badname
+adduser --disabled-password --gecos "" aticha.sun --force-badname
+adduser --disabled-password --gecos "" aye.aye --force-badname
+adduser --disabled-password --gecos "" lukas.sta --force-badname
+adduser --disabled-password --gecos "" michal.zie --force-badname
+adduser --disabled-password --gecos "" sahassawat.wan --force-badname
+adduser --disabled-password --gecos "" karit.cha --force-badname
+adduser --disabled-password --gecos "" chirayu.phr --force-badname
+adduser --disabled-password --gecos "" natnarong.tre --force-badname
+adduser --disabled-password --gecos "" thethawat.kon --force-badname
+adduser --disabled-password --gecos "" panupong.tan --force-badname
+adduser --disabled-password --gecos "" aukkrit.soi --force-badname
+adduser --disabled-password --gecos "" prapon.tok --force-badname
+
+#Set password to user's Student ID
+
+echo -e "62110700602\n62110700602" | passwd natchaphon.raj
+echo -e "62110700603\n62110700603" | passwd thanyawee.sri
+echo -e "62110700604\n62110700604" | passwd yasumin.ker
+echo -e "62110700605\n62110700605" | passwd nachol.tae
+echo -e "62110800801\n62110800801" | passwd benjaporn.bhu
+echo -e "62110800801\n62110800802" | passwd athisri.sit
+echo -e "61130500201\n61130500201" | passwd kritamate.kam
+echo -e "61130500202\n61130500202" | passwd krittiya.onb
+echo -e "61130500203\n61130500203" | passwd kantapon.ruj
+echo -e "61130500204\n61130500204" | passwd krerkthad.kun
+echo -e "61130500205\n61130500205" | passwd kiatisak.pet
+echo -e "61130500206\n61130500206" | passwd charuphat.chi
+echo -e "61130500207\n61130500207" | passwd chaichet.pha
+echo -e "61130500208\n61130500208" | passwd nuttawut.pro
+echo -e "61130500209\n61130500209" | passwd natthida.tha
+echo -e "61130500210\n61130500210" | passwd tawan.von
+echo -e "61130500211\n61130500211" | passwd taipun.ban
+echo -e "61130500212\n61130500212" | passwd tanadon.say
+echo -e "61130500213\n61130500213" | passwd teeranai.kon
+echo -e "61130500214\n61130500214" | passwd tianwit.mou
+echo -e "61130500215\n61130500215" | passwd preeyawarang.pon
+echo -e "61130500216\n61130500216" | passwd pongpoom.pan
+echo -e "61130500217\n61130500217" | passwd patthamaporn.sam
+echo -e "61130500218\n61130500218" | passwd pongsakorn.cha
+echo -e "61130500219\n61130500219" | passwd phruksachat.yao
+echo -e "61130500220\n61130500220" | passwd pittawat.tav
+echo -e "61130500223\n61130500223" | passwd phuritat.cha
+echo -e "61130500224\n61130500224" | passwd vorapol.cha
+echo -e "61130500225\n61130500225" | passwd vorawut.per
+echo -e "61130500226\n61130500226" | passwd warakorn.cha
+echo -e "61130500227\n61130500227" | passwd waritnan.soo
+echo -e "61130500228\n61130500228" | passwd wisa.moo
+echo -e "61130500229\n61130500229" | passwd sakdipat.ont
+echo -e "61130500230\n61130500230" | passwd supapatr.won
+echo -e "61130500231\n61130500231" | passwd anawat.pao
+echo -e "61130500232\n61130500232" | passwd audsadawut.suk
+echo -e "61130500233\n61130500233" | passwd aekknon.pho
+echo -e "61130500234\n61130500234" | passwd kongphob.won
+echo -e "61130500235\n61130500235" | passwd kani-on.pan
+echo -e "61130500236\n61130500236" | passwd chantisa.san
+echo -e "61130500238\n61130500238" | passwd thitaree.kem
+echo -e "61130500239\n61130500239" | passwd napat.jam
+echo -e "61130500240\n61130500240" | passwd thanadon.tee
+echo -e "61130500241\n61130500241" | passwd noppanut.boo
+echo -e "61130500242\n61130500242" | passwd preeyapat.sut
+echo -e "61130500244\n61130500244" | passwd pornphat.sro
+echo -e "61130500245\n61130500245" | passwd wicharn.rue
+echo -e "61130500246\n61130500246" | passwd sinin.cha
+echo -e "61130500248\n61130500248" | passwd aticha.sun
+echo -e "61130500249\n61130500249" | passwd aye.aye
+echo -e "61130500251\n61130500251" | passwd lukas.sta
+echo -e "61130500252\n61130500252" | passwd michal.zie
+echo -e "61130500253\n61130500253" | passwd sahassawat.wan
+echo -e "61130500254\n61130500254" | passwd karit.cha
+echo -e "61130500255\n61130500255" | passwd chirayu.phr
+echo -e "61130500256\n61130500256" | passwd natnarong.tre
+echo -e "61130500257\n61130500257" | passwd thethawat.kon
+echo -e "61130500258\n61130500258" | passwd panupong.tan
+echo -e "61130500259\n61130500259" | passwd aukkrit.soi
+echo -e "61130500261\n61130500261" | passwd prapon.tok
+
+# Force password change on user ssh login
+
+passwd -e natchaphon.raj
+passwd -e thanyawee.sri
+passwd -e yasumin.ker
+passwd -e nachol.tae
+passwd -e benjaporn.bhu
+passwd -e athisri.sit
+passwd -e kritamate.kam
+passwd -e krittiya.onb
+passwd -e kantapon.ruj
+passwd -e krerkthad.kun
+passwd -e kiatisak.pet
+passwd -e charuphat.chi
+passwd -e chaichet.pha
+passwd -e nuttawut.pro
+passwd -e natthida.tha
+passwd -e tawan.von
+passwd -e taipun.ban
+passwd -e tanadon.say
+passwd -e teeranai.kon
+passwd -e tianwit.mou
+passwd -e preeyawarang.pon
+passwd -e pongpoom.pan
+passwd -e patthamaporn.sam
+passwd -e pongsakorn.cha
+passwd -e phruksachat.yao
+passwd -e pittawat.tav
+passwd -e phuritat.cha
+passwd -e vorapol.cha
+passwd -e vorawut.per
+passwd -e warakorn.cha
+passwd -e waritnan.soo
+passwd -e wisa.moo
+passwd -e sakdipat.ont
+passwd -e supapatr.won
+passwd -e anawat.pao
+passwd -e audsadawut.suk
+passwd -e aekknon.pho
+passwd -e kongphob.won
+passwd -e kani-on.pan
+passwd -e chantisa.san
+passwd -e thitaree.kem
+passwd -e napat.jam
+passwd -e thanadon.tee
+passwd -e noppanut.boo
+passwd -e preeyapat.sut
+passwd -e pornphat.sro
+passwd -e wicharn.rue
+passwd -e sinin.cha
+passwd -e aticha.sun
+passwd -e aye.aye
+passwd -e lukas.sta
+passwd -e michal.zie
+passwd -e sahassawat.wan
+passwd -e karit.cha
+passwd -e chirayu.phr
+passwd -e natnarong.tre
+passwd -e thethawat.kon
+passwd -e panupong.tan
+passwd -e aukkrit.soi
+passwd -e prapon.tok
+
+# Add Groups
+
+groupadd grp_1
+groupadd grp_2
+groupadd grp_3
+groupadd grp_4
+groupadd grp_5
+groupadd grp_6
+groupadd grp_7
+groupadd grp_8
+groupadd grp_9
+groupadd grp_10
+groupadd grp_11
+groupadd grp_12
+groupadd grp_13
+groupadd grp_14
+groupadd grp_15
+groupadd grp_16
+groupadd grp_17
+groupadd grp_18
+
+# Add users to appropriate group
+
+gpasswd -a kantapon.ruj grp_1
+gpasswd -a chantisa.san grp_1
+gpasswd -a michal.zie grp_1
+gpasswd -a kiatisak.pet grp_2
+gpasswd -a chaichet.pha grp_2
+gpasswd -a natnarong.tre grp_2
+gpasswd -a preeyawarang.pon grp_3
+gpasswd -a pongsakorn.cha grp_3
+gpasswd -a anawat.pao grp_3
+gpasswd -a teeranai.kon grp_4
+gpasswd -a phuritat.cha grp_4
+gpasswd -a supapatr.won grp_4
+gpasswd -a pongpoom.pan grp_5
+gpasswd -a sinin.cha grp_5
+gpasswd -a aticha.sun grp_5
+gpasswd -a nuttawut.pro grp_6
+gpasswd -a napat.jam grp_6
+gpasswd -a thethawat.kon grp_6
+gpasswd -a vorawut.per grp_7
+gpasswd -a aekknon.pho grp_7
+gpasswd -a panupong.tan grp_7
+gpasswd -a natchaphon.raj grp_8
+gpasswd -a yasumin.ker grp_8
+gpasswd -a athisri.sit grp_8
+gpasswd -a thanyawee.sri grp_9
+gpasswd -a nachol.tae grp_9
+gpasswd -a benjaporn.bhu grp_9 
+gpasswd -a taipun.ban grp_10
+gpasswd -a phruksachat.yao grp_10
+gpasswd -a kani-on.pan grp_10
+gpasswd -a preeyapat.sut grp_10
+gpasswd -a kritamate.kam grp_11
+gpasswd -a noppanut.boo grp_11
+gpasswd -a aukkrit.soi grp_11
+gpasswd -a tawan.von grp_12
+gpasswd -a tanadon.say grp_12
+gpasswd -a audsadawut.suk grp_12
+gpasswd -a natthida.tha grp_13
+gpasswd -a patthamaporn.sam grp_13
+gpasswd -a aye.aye grp_13
+gpasswd -a vorapol.cha grp_14
+gpasswd -a kongphob.won grp_14
+gpasswd -a chirayu.phr grp_14
+gpasswd -a sakdipat.ont grp_15
+gpasswd -a thitaree.kem grp_15
+gpasswd -a pornphat.sro grp_15
+gpasswd -a charuphat.chi grp_16
+gpasswd -a waritnan.soo grp_16
+gpasswd -a sahassawat.wan grp_16
+gpasswd -a wisa.moo grp_17
+gpasswd -a karit.cha grp_17
+gpasswd -a prapon.tok grp_17
+gpasswd -a krerkthad.kun grp_18
+gpasswd -a tianwit.mou grp_18
+gpasswd -a lukas.sta grp_18
+gpasswd -a wicharn.rue sudo
+gpasswd -a pittawat.tav sudo
+gpasswd -a thanadon.tee sudo
+gpasswd -a krittiya.onb sudo
+gpasswd -a warakorn.cha sudo
